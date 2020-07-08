bd6c643a1c606f3e1d161aabcf12c1ad87369cd5
RGoslow <65988595+RGoslow@users.noreply.github.com>
2020-07-08 10:54:44 -0400

    Add entry logic
    Program now allows three entries per player and checks the number of players before moving on.

--- charadesgame.py
+++ charadesgame.py
@@ -82,42 +82,63 @@ def play_game():
     #what happens when buttons are clicked
     #clicking submit adds name + phrases to lists
     def submit_click(self):
-        if name_entry.get() != "" and len(phrase_entry.get().split()) <= 5 and phrase_entry.get() != "":
-            names.append(name_entry.get())
-            phrases.append(phrase_entry.get())
-            game_message["text"]="Phrase submitted!"
-        elif len(phrase_entry.get().split()) > 5 or phrase_entry.get() == "":
-            game_message["text"]="Phrases must be 1-5 words."
-        elif name_entry.get() == "":
-            game_message["text"]="Blank entries not allowed."
-        
+        if len(names) >= 10:
+            game_message["text"]="You already have 10 players."
+        else:
+            if name_entry.get() != "" and len(phrase_entry1.get().split()) <= 5 and phrase_entry1.get() != "" and len(phrase_entry2.get().split()) <= 5 and phrase_entry2.get() != "" and len(phrase_entry3.get().split()) <= 5 and phrase_entry3.get() != "":
+                names.append(name_entry.get())
+                phrases.append(phrase_entry1.get())
+                phrases.append(phrase_entry2.get())
+                phrases.append(phrase_entry3.get())
+                game_message["text"]="Phrases submitted!"
+                name_entry.delete(0, tk.END)
+                phrase_entry1.delete(0, tk.END)
+                phrase_entry2.delete(0, tk.END)
+                phrase_entry3.delete(0, tk.END)
 
+            elif len(phrase_entry1.get().split()) > 5 or phrase_entry1.get() == "" or len(phrase_entry2.get().split()) > 5 or phrase_entry2.get() == "" or len(phrase_entry3.get().split()) > 5 or phrase_entry3.get() == "":
+                game_message["text"]="All phrases must be 1-5 words."
+            elif name_entry.get() == "":
+                game_message["text"]="You need to enter a name."
         print(names)
         print(phrases)
-
+    def move_on_click(self):
+        if len(names) == 6 or len(names) == 8 or len(names) == 10:
+            game_message["text"]="You're ready to move on."
+        else:
+            game_message["text"]="You need more players."
     #create widgets
     game_label = tk.Label(master=window, text="Enter Information")
     game_message = tk.Label(master=window,text="")
     fr_entry = tk.Frame(master=window, bg="white")
     name_label = tk.Label(master=fr_entry, text="Name:")
     name_entry = tk.Entry(master=fr_entry)
-    phrase_label = tk.Label(master=fr_entry, text="Secret Phrase:")
-    phrase_entry = tk.Entry(master=fr_entry)
+    phrase_label1 = tk.Label(master=fr_entry, text="Secret Phrase:")
+    phrase_label2 = tk.Label(master=fr_entry, text="Secret Phrase:")
+    phrase_label3 = tk.Label(master=fr_entry, text="Secret Phrase:")
+    phrase_entry1 = tk.Entry(master=fr_entry)
+    phrase_entry2 = tk.Entry(master=fr_entry)
+    phrase_entry3 = tk.Entry(master=fr_entry)
     submit_button = tk.Button(master=window, text="Submit")
+    move_on_button = tk.Button(master=window, text="Move On")
     #place entry widgets in entry frame
     name_label.grid(row="0", column="0", sticky="e")
     name_entry.grid(row="0", column="1")
-    phrase_label.grid(row="1",column="0", sticky="e")
-    phrase_entry.grid(row="1", column="1", sticky="e")
+    phrase_label1.grid(row="1",column="0", sticky="e")
+    phrase_entry1.grid(row="1", column="1", sticky="w")
+    phrase_label2.grid(row="2",column="0", sticky="e")
+    phrase_entry2.grid(row="2", column="1", sticky="w")
+    phrase_label3.grid(row="3",column="0", sticky="e")
+    phrase_entry3.grid(row="3", column="1", sticky="w")
     #place all widgets
-    game_label.pack()
-    fr_entry.pack()
-    game_message.pack()
-    submit_button.pack()
-
+    game_label.grid(row="0", column="0",columnspan="2")
+    fr_entry.grid(row="1", column="0",columnspan="2")
+    game_message.grid(row="2", column="0",columnspan="2")
+    submit_button.grid(row="3", column="0")
+    move_on_button.grid(row="3",column="1")
     #bind button
     submit_button.bind("<Button-1>", submit_click)
-
+    move_on_button.bind("<Button-1>",move_on_click)
 
 
 
