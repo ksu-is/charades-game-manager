dcce3da896a2471cb4370e42751057e67be36cbc
RGoslow <65988595+RGoslow@users.noreply.github.com>
2020-07-07 22:10:12 -0400

    Begin adding playable functionality
    Added a first screen for the game which has widgets for name and phrase entry. (No interactivity yet, though.) Also added comments for increased documentation.

--- charadesgame.py
+++ charadesgame.py
@@ -29,29 +29,65 @@ def title_screen():
     title.grid(row="0", column="0")
     fr_buttons.grid(row="1", column="0")
 
+
+    #bind buttons
+    play_button.bind("<Button-1>", play_click)
     rules_button.bind("<Button-1>", rules_click)  
     credits_button.bind("<Button-1>", credits_click)
+
+#function that creates credits screen
 def credits_screen():
     #clear screen first
     for widget in window.winfo_children():
         widget.destroy()
 
-    credits_label = tk.Label(master=window,text="Credits")
+    #create widgets
+    credits_label = tk.Label(master=window,text="Credits",fg="#FFC0CB")
     credits_label["font"] = font.Font(size="24", weight="bold")
     credits_text = tk.Label(master=window, text="Coding: Raymond Goslow\nTimer: Ostcrom\nRules: Carson Claud\n\nFor IS3020 Summer 2020\nKennesaw State University")
     home_button = tk.Button(master=window,text="Back")
+    #place all widgets
     credits_label.pack()
     credits_text.pack()
     home_button.pack()
+
+    #bind buttons
     home_button.bind("<Button-1>", home_click)
 
+#function that plays game
+def play_game():
+    #clear screen first
+    for widget in window.winfo_children():
+        widget.destroy()
+
+    #create widgets
+    game_label = tk.Label(master=window, text="Enter Information")
+    game_message = tk.Label(master=window,text="")
+    fr_entry = tk.Frame(master=window, bg="white")
+    name_label = tk.Label(master=fr_entry, text="Name:")
+    name_entry = tk.Entry(master=fr_entry)
+    phrase_label = tk.Label(master=fr_entry, text="Secret Phrase:")
+    phrase_entry = tk.Entry(master=fr_entry)
+    #place entry widgets in entry frame
+    name_label.grid(row="0", column="0", sticky="e")
+    name_entry.grid(row="0", column="1")
+    phrase_label.grid(row="1",column="0", sticky="e")
+    phrase_entry.grid(row="1", column="1", sticky="e")
+    #place all widgets
+    game_label.pack()
+    fr_entry.pack()
+    game_message.pack()
 #what happens when buttons are clicked
+#clicking play button starts game
+def play_click(self):
+    play_game()
 #clicking rules button opens rules in pastebin
 def rules_click(self):
     webbrowser.open("https://pastebin.com/WPXtPBig")
-#clicking credits button
+#clicking credits button opens credits
 def credits_click(self):
     credits_screen()
+#clicking home button returns to home
 def home_click(self):
     title_screen()
   
